2a6d78e60f4d35b7cb0e7299b5801ea444986d4d
Tomas Farias <85124666+TomasFarias11@users.noreply.github.com>
2022-02-11 11:31:54 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
 require('./db.js');
@@ -12,10 +11,10 @@ const cors = require('cors')
 server.name = 'API';
 
 server.use(cors())
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan('dev'));
+// server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
